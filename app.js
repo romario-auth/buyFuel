d2988143330567c07aaa884cbba965e9a59648a2
Romario <romario.auth@gmail.com>
2020-09-16 19:29:07 -0300

    Dependencies installed

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+const PORT = 3000;
+
+app.listen(PORT, function(){
+    console.log(`O express está ok na porta ${PORT}`);
+});
+
+app.get('/', (req, res) => {
+    res.send("Está funcionando");
+})
+
+// npm install express express-handlebars body-parser sequelize mysql
